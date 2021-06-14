985e1dcb32a178ec3297920b46319f5626df4759
Florian-0tt <florianottj@icloud.com>
2021-06-14 11:53:36 +0200

    parsetree

--- t4_parsetree.c
+++ t4_parsetree.c
@@ -5,23 +5,29 @@
 #include "t4_header.h"
 #include "t4_parser_gen.tab.h"
 
-
-int del = 1; /* distance of graph columns */
-int eps = 3; /* distance of graph lines */
+#define lmax 200
+#define cmax 200
+char graph[lmax][cmax];
+char leer = ' ';
+int line = 0;
 int graphennummer = 0;
 
-void nodetest(NodeType *p,int l,int c);
-
+void initial();
+void drawline(int l, int c);
+void draw(char *s, int l, int c);
+void ausgabe();
+void nodetest(NodeType *p,int c);
 
 int ex (NodeType *p) {
-	nodetest(p,0,0);
+	initial();
+	line = 0;
+	nodetest(p,0);
+	ausgabe();
 	return 0;
 }
 
-
 void nodetest(
 NodeType *p,
-int l,
 int c
 ){
 	char* s;
@@ -78,42 +84,98 @@ int c
 					break;
 			}
 	}
-		
 	
-	if(l==0&&c==0)
+	if(line==0&&c==0)
 	{	
 		if(s!="[  ]"){
-			printf("\n");
-			printf("Graph %d:\n\n",graphennummer);
-			printf("%s\n",s);
-			graphennummer++;
-			l++;
+			draw(s,line,c);
+			c--;
 		}
-	}else if(l>0&&c>0)
-	{
+		
+	}else if(line>0&&c>0)	{
+		int d = c;
+		char seq[20]="???";
+		sprintf(seq," >----%s",s);
+		s = seq;
+		
 		for(int x = 1;x<c;x++){
-			printf(" |  ");printf("   ");
+			d=d+3;
 		}
-		printf(" >----%s\n",s);
+		draw(s,line,d);
+		
 	}else{
-		printf(" |  ");
+		int d = c;
+		char seq[20]="???";
+		sprintf(seq," >----%s",s);
+		s = seq;
 		if(c>1){
 			for(int i = 0;i<c;i++){
-				printf("   ");
+				d=d+3;
 			}
 		}
-		printf("%s\n",s);
-		l++;
+		draw(s,line,d);
 	}
 	
 	if(p->type==type_operator){
 		if (p->opr.nops > 0)
-			nodetest(p->opr.op[0],l,c+1);
+			nodetest(p->opr.op[0],c+2);
 		if (p->opr.nops > 1)
-			nodetest(p->opr.op[1],l,c+1);
+			nodetest(p->opr.op[1],c+2);
 		if (p->opr.nops > 2)
-			nodetest(p->opr.op[2],l,c+1);
+			nodetest(p->opr.op[2],c+2);
 	}
-	
-	
+}
+
+void initial(){
+	for(int x = 0; x<=lmax; x++){
+		for(int y = 0; y<=cmax; y++){
+			graph[x][y]=leer;	
+		}
+	}
+}
+
+void drawline(int l, int c){
+	for(int x = l-1; x >= 0 ; x--){
+		if(graph[x][c]!=leer)
+			break;
+		graph[x][c]='|';
+	}
+}
+
+void draw(char *s, int l, int c){
+	int size = 0;
+	for(int x = 0; s[x]!='\0';x++){
+		size++;
+	}	
+	for(int y = 0; y <= size; y++){
+		if((c+y)<=cmax)
+			graph[line][c+y]=s[y];
+	}
+	line=line +1;
+	drawline(line,c);
+}
+
+void ausgabe(){
+	if(graph[0][0]!=leer)
+		printf("Graph: %d:\n\n",graphennummer++);
+	for(int x = 0; x < lmax; x++){
+
+		int clast = cmax;
+		for(clast; clast>=0; clast--){
+			if(graph[x][clast]!=leer)
+			{	
+				clast++;
+				break;
+			}
+		}
+		
+		int y = 0;
+		for(y; y < clast; y++){
+			printf("%c",graph[x][y]);
+		}
+		if(y != 0){
+			printf("\n");
+		}
+	}
+	printf("\n");
 }
